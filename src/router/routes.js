9f5cf603a23829fad212be6fd06a12652d03d2a2
josh-boat365 <nyarko.boateng20@outlook.com>
2025-12-03 16:02:14 +0000

    added new routes for new pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,15 +1,66 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHistory
+} from "vue-router";
 import Login from "@/pages/auth/Login.vue";
 import Register from "@/pages/auth/Register.vue";
 import Dashboard from "@/pages/dashboard/Index.vue";
+import AuthTrails from "@/pages/dashboard/AuthTrails.vue";
+import Settings from "@/pages/dashboard/Settings.vue";
+import CreateUser from "@/pages/users/Create.vue";
+import CreatePrivileges from "@/pages/privileges/Index.vue";
+import CreateSystemPasswordCategory from "@/pages/system-setup/CreateSystemPasswordCategory.vue";
+import CreateSystemPasswordPolicyCategory from "@/pages/system-setup/CreateSystemPasswordPolicyCategory.vue";
+import CreateSystemPassword from "@/pages/system-setup/Index.vue";
 
 const routes = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {path: "/", component: Login},
-    {path: "/login", component: Login},
-    {path: "/register", component: Register},
-    {path: "/dashboard", component: Dashboard},
-]});
+  history: createWebHistory(
+    import.meta.env.BASE_URL),
+  routes: [{
+      path: "/",
+      component: Login
+    },
+    {
+      path: "/login",
+      component: Login
+    },
+    {
+      path: "/register",
+      component: Register
+    },
+    {
+      path: "/dashboard",
+      component: Dashboard
+    },
+    {
+      path: "/dashboard/auth-trails",
+      component: AuthTrails
+    },
+    {
+      path: "/dashboard/settings",
+      component: Settings
+    },
+    {
+      path: "/dashboard/create-user",
+      component: CreateUser
+    },
+    {
+      path: "/dashboard/create-privileges",
+      component: CreatePrivileges
+    },
+    {
+      path: "/dashboard/create-system-passwords-category",
+      component: CreateSystemPasswordCategory
+    },
+    {
+      path: "/dashboard/create-system-passwords-policy-category",
+      component: CreateSystemPasswordPolicyCategory
+    },
+    {
+      path: "/dashboard/create-system-passwords",
+      component: CreateSystemPassword
+    },
+  ]
+});
 
 export default routes;
