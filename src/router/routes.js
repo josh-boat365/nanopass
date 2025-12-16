621c64e14cf493f97543ec95d62f9c7a891fe0a1
josh-boat365 <nyarko.boateng20@outlook.com>
2025-12-16 14:59:40 +0000

    Updated routes for newly added pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,6 +6,9 @@ import Login from "@/pages/auth/Login.vue";
 import Register from "@/pages/auth/Register.vue";
 import Dashboard from "@/pages/dashboard/Index.vue";
 import UserDashboard from "@/pages/dashboard/user/Index.vue";
+import UserDashboard2 from "@/pages/dashboard/user/Index2.vue";
+import UserAssignedKeys from "@/pages/dashboard/user/AssignedKeys.vue";
+import UserPersonalKeys from "@/pages/dashboard/user/PersonalKeys.vue";
 import AuthTrails from "@/pages/dashboard/AuthTrails.vue";
 import Settings from "@/pages/dashboard/Settings.vue";
 import CreateUser from "@/pages/users/Create.vue";
@@ -29,21 +32,34 @@ const routes = createRouter({
       component: Register
     },
     {
-      path: "/admin/dashboard",
-      component: Dashboard
-    },
-    {
       path: "/user/dashboard",
       component: UserDashboard
     },
     {
-      path: "/admin/dashboard/auth-trails",
-      component: AuthTrails
+      path: "/user/dashboard2",
+      component: UserDashboard2
+    },
+    {
+      path: "/user/assigned-keys",
+      component: UserAssignedKeys
+    },
+    {
+      path: "/user/personal-keys",
+      component: UserPersonalKeys
     },
     {
       path: "/dashboard/settings",
       component: Settings
     },
+    {
+      path: "/admin/dashboard",
+      component: Dashboard
+    },
+    {
+      path: "/admin/dashboard/auth-trails",
+      component: AuthTrails
+    },
+    
     {
       path: "/admin/dashboard/create-user",
       component: CreateUser
