01a92703c72f26e496cef7439338b73b68a2b51d
josh-boat365 <nyarko.boateng20@outlook.com>
2025-12-16 14:47:37 +0000

    Created an AppFooter

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,9 @@
+<script setup>
+const currentYear = new Date().getFullYear();
+</script>
+
+<template>
+    <footer class="border-t bg-white p-2 text-center text-sm text-gray-500">
+        &copy; 2025 - <span>{{ currentYear }}</span> NanoPass. All rights reserved. Developed with ❤️ by IT BPSL Team.
+    </footer>
+</template>
